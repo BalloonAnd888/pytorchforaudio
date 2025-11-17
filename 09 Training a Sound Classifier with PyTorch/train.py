5788f468ef844ceb0eecfc7ae7a6c9c04d0049a2
Andrew Lau <82020841+BalloonAnd888@users.noreply.github.com>
2025-11-17 01:33:27 -0800

    Training a Sound Classifier with PyTorch

---
+++ 09 Training a Sound Classifier with PyTorch/train.py
@@ -0,0 +1,80 @@
+import torch
+import torchaudio
+from torch import nn
+from torch.utils.data import DataLoader
+from urbansounddataset import UrbanSoundDataset
+from cnn import CNNNetwork
+
+BATCH_SIZE = 128
+EPOCHS = 10
+LEARNING_RATE = .001
+
+ANNOTATIONS_FILE = "C:/Users/alau2/Documents/datasets/UrbanSound8K/metadata/UrbanSound8K.csv"
+AUDIO_DIR = "C:/Users/alau2/Documents/datasets/UrbanSound8K/audio"
+SAMPLE_RATE = 22050
+NUM_SAMPLES = 22050
+
+def create_data_loader(train_data, batch_size):
+    train_dataloader = DataLoader(train_data, batch_size=batch_size)
+    return train_dataloader
+
+def train_one_epoch(model, data_loader, loss_fn, optimizer, device):
+    for inputs, targets in data_loader:
+        inputs, targets = inputs.to(device), targets.to(device)
+
+        # calculate loss
+        predictions = model(inputs)
+        loss = loss_fn(predictions, targets)
+
+        # backpropagate loss and update weights
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print(f"Loss: {loss.item()}")
+
+def train(model, data_loader, loss_fn, optimizer, device, epochs):
+    for i in range(epochs):
+        print(f"Epoch {i+1}")
+        train_one_epoch(model, data_loader, loss_fn, optimizer, device)
+        print("--------------------")
+    print("Training is done.")
+
+if __name__ == "__main__":
+    if torch.cuda.is_available():
+        device = "cuda"
+    else:
+        device = "cpu"
+    print(f"Using {device} device")
+
+    # instantiating our dataset object and create data loader
+    mel_spectrogram = torchaudio.transforms.MelSpectrogram(
+        sample_rate=SAMPLE_RATE, 
+        n_fft=1024,
+        hop_length=512,
+        n_mels=64
+    )
+
+    usd = UrbanSoundDataset(ANNOTATIONS_FILE, 
+                            AUDIO_DIR, 
+                            mel_spectrogram, 
+                            SAMPLE_RATE, 
+                            NUM_SAMPLES, 
+                            device)
+
+    train_dataloader = create_data_loader(usd, BATCH_SIZE)
+
+    # construct model and assign it to device
+    cnn = CNNNetwork().to(device)
+    print(cnn)
+
+    # instantiate loss function and optimizer
+    loss_fn = nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(cnn.parameters(), lr=LEARNING_RATE)
+
+    # train model
+    train(cnn, train_dataloader, loss_fn, optimizer, device, EPOCHS)
+
+    # save model
+    torch.save(cnn.state_dict(), "feedforwardnet.pth")
+    print("Model trained and stored at feedforwardnet.pth")
